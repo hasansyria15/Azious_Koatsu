ed4aae4ac0bbae3b48eb5f0aeec63ac15fc6df48
hasansyria15 <haald001@edu.cegepgarnea.ca>
2025-11-19 21:12:09 -0500

    configuration de static

--- Koatsu/Koatsu/settings.py
+++ Koatsu/Koatsu/settings.py
@@ -124,12 +124,14 @@ ALLOWED_HOSTS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'Site' / 'static',
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
